ff61a4beded48ab3acd43de4d31296f2589e3c16
hasanpasha <bashahsn22@gmail.com>
2024-10-22 15:26:06 +0300

    cleanup

--- lib/core/presentation/providers/video_player_controller_provider.dart
+++ lib/core/presentation/providers/video_player_controller_provider.dart
@@ -2,9 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:media_kit/media_kit.dart' as media_kit;
 import 'package:media_kit_video/media_kit_video.dart' show VideoController;
 
-import 'subtitle_provider.dart';
-import 'video_provider.dart';
-
 class VideoPlayerNotifier extends StateNotifier<media_kit.Player> {
   VideoPlayerNotifier() : super(createPlayer());
 
@@ -49,41 +46,6 @@ final videoPlayerProvider =
 final videoPlayerPlayingStateProvider = StreamProvider<bool>(
   (ref) => ref.watch(videoPlayerProvider).stream.playing,
 );
-
-// final mediaVideoOpenerControllerWrapperProvider =
-//     FutureProvider<VideoController>((ref) async {
-//   final videoPlayer = ref.watch(videoPlayerProvider);
-//   final video = ref.watch(videoProvider);
-
-//   if (video == null) {
-//     await videoPlayer.stop();
-//   } else {
-//     await videoPlayer.open(video);
-//   }
-
-//   final controller = VideoController(
-//     videoPlayer,
-//   );
-
-//   return controller;
-// });
-
-// final mediaSubtitleSetterControllerWrapperProvider =
-//     Provider<VideoController>((ref) async {
-//   final controllerValue = ref.watch(mediaVideoOpenerControllerWrapperProvider);
-//   final subtitle = ref.watch(subtitleProvider.notifier).addListener(listener);
-
-//   final controller = controllerValue.value;
-
-//   final videoPlayer = ref.read(videoPlayerProvider);
-//   if (subtitle != null) {
-//     await videoPlayer.setSubtitleTrack(subtitle);
-//   } else {
-//     await videoPlayer.setSubtitleTrack(media_kit.SubtitleTrack.no());
-//   }
-
-//   return controller;
-// });
 
 final videoPlayerControllerProvider = Provider<VideoController>(
   (ref) {
